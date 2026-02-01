a2d9e02f99e80eb58b609d66699c2ae5788febef
why_code <119089018+VWiman@users.noreply.github.com>
2026-02-01 14:08:45 +0100

    import is ci function, made dataset download CI-safe and local-dev compatible

--- src/bellikas_retro/utils/__init__.py
+++ src/bellikas_retro/utils/__init__.py
@@ -1,8 +1,9 @@
-from .helpers import _project_root
+from .helpers import _project_root, _is_ci
 from .config import PROJECT_ROOT, CSV_NAME, DATA_DIR, KAGGLE_DATASET_PATH
 
 __all__ = [
     "_project_root",
+    "_is_ci",
     "PROJECT_ROOT",
     "CSV_NAME",
     "DATA_DIR",
